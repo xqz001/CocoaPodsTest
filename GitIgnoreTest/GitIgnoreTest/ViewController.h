1a646c872c7a657f8f0f74b0178701751ba9f964
liwei <liwei@mapbar.com>
2016-03-07 14:19:08 +0800

    init project

---
+++ GitIgnoreTest/GitIgnoreTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitIgnoreTest
+//
+//  Created by liwei on 16/3/7.
+//  Copyright © 2016年 com.test.ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitIgnoreTest/GitIgnoreTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitIgnoreTest
+//
+//  Created by liwei on 16/3/7.
+//  Copyright © 2016年 com.test.ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
